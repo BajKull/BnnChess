9e5c0d4a9c3252ba2fd4b2473ff9b98fae416c8b
Dawid Banachowski <bajkull@gmail.com>
2023-03-23 21:06:53 +0100

    timer bar

--- src/views/chat/ChatPanel.tsx
+++ src/views/chat/ChatPanel.tsx
@@ -4,13 +4,19 @@ import { useGameStore } from "@/store/gameStore";
 import React from "react";
 import GameSettings from "../gameSettings/GameSettings";
 import ChatMoves from "./ChatMoves";
+import TimeBar from "./TimeBar";
 
 const ChatPanel = () => {
   const gameState = useGameStore((state) => state.gameState);
   return (
     <>
       {gameState === "settings" && <GameSettings />}
-      {gameState === "game" && <ChatMoves />}
+      {gameState === "game" && (
+        <>
+          <ChatMoves />
+          <TimeBar />
+        </>
+      )}
       {/* {gameState === 'results' && <GameSettings />} */}
     </>
   );
---
+++ src/views/chat/TimeBar.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useGameStore } from "@/store/gameStore";
+import classNames from "classnames";
+import React from "react";
+import cls from "./timeBar.module.scss";
+import ReactDOM from "react-dom";
+import { useChatStore } from "@/store/chatMoves";
+
+const TimeBar = () => {
+  const moveTime = useGameStore((state) => state.moveTime);
+  const isChatTurn = useChatStore((state) => state.isChatTurn);
+
+  const clsDiv = classNames(
+    cls.timeBar,
+    "fixed top-0 left-0 h-1 bg-purple-800"
+  );
+
+  if (!isChatTurn) return null;
+  if (typeof document === "undefined") return null;
+  return ReactDOM.createPortal(
+    <div className={clsDiv} style={{ animationDuration: `${moveTime}s` }} />,
+    document.body
+  );
+};
+
+export default TimeBar;
