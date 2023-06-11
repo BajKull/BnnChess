eca451c1fe24f71190463e7911055e7b10bf12d5
Dawid Banachowski <bajkull@gmail.com>
2023-06-11 22:46:54 +0200

    prevent displaying the same toast error

--- src/views/chat/ChatPanel.tsx
+++ src/views/chat/ChatPanel.tsx
@@ -3,8 +3,8 @@
 import useChessActions from "@/hooks/useChessActions";
 import { useChatStore } from "@/store/chatMovesStore";
 import { useGameStore } from "@/store/gameStore";
-import React, { useEffect } from "react";
-import { toast } from "react-toastify";
+import React, { useEffect, useRef } from "react";
+import { Id, toast } from "react-toastify";
 import GameSettings from "../gameSettings/GameSettings";
 import ChatMoves from "./ChatMoves";
 import TimeBar from "./TimeBar";
@@ -16,6 +16,8 @@ const ChatPanel = () => {
   const chatMoveRef = useChatStore((state) => state.chatMoveRef);
   const resetTimerTrigger = useChatStore((state) => state.resetTimerTrigger);
 
+  const toastId = useRef<Id>("");
+
   const { move } = useChessActions();
 
   useEffect(() => {
@@ -25,10 +27,14 @@ const ChatPanel = () => {
       setTimeout(() => {
         if (!chatMoveRef.current) {
           waitForMove();
-          toast.error("No legal moves were made in chat. Vote again.", {
-            autoClose: 4000,
-          });
           resetTimerTrigger();
+          if (toast.isActive(toastId.current)) return;
+          toastId.current = toast.error(
+            "No legal moves were made in chat. Vote again.",
+            {
+              autoClose: 4000,
+            }
+          );
           return;
         }
         const success = move(chatMoveRef.current);
