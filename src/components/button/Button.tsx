858f4f28154304af4d0b6738ca36afe701d702cd
Dawid Banachowski <bajkull@gmail.com>
2023-06-14 15:02:08 +0200

    button sizes

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -4,21 +4,27 @@ import React from "react";
 interface IProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   primary?: boolean;
   secondary?: boolean;
+  size?: "small" | "medium" | "large" | "xl";
 }
 
 const Button = ({
   children,
   className,
+  size,
   primary,
   secondary,
   ...props
 }: IProps) => {
   const clsBtn = classNames(
-    "border-0 rounded-md text-white px-3 py-2 text-sm font-semibold",
+    "border-0 rounded-md text-white font-semibold",
     className,
     {
       "bg-zinc-800 hover:bg-zinc-700 focus:bg-zinc-700": secondary,
       "bg-purple-800 hover:bg-purple-700 focus:bg-purple-700": primary,
+      "text-sm px-3 py-2": size === "small",
+      "text-sm px-4 py-2": size === "medium" || !size,
+      "px-5 py-2": size === "large",
+      "text-lg px-8 py-3": size === "xl",
     }
   );
   return (
